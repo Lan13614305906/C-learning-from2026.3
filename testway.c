a519879703d0433966e34a5083ff0c16ccfbc331
Lan13614305906 <3585551928@qq.com>
2026-03-16 21:55:36 +0800

    测试上传路径

---
+++ testway.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+int main() {
+    // 定义两个整数
+    int a = 10;
+    int b = 20;
+    int sum = a + b;
+
+    // 输出结果
+    printf("a = %d\n", a);
+    printf("b = %d\n", b);
+    printf("a + b = %d\n", sum);
+
+    return 0;
+}
